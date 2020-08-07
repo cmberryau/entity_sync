e490b477551841088707a0ad827f17d1473564a4
Chris Berry <chris.berry@lodlaw.com>
2020-08-07 11:30:11 +1000

    syncing results work

--- lib/src/sync.dart
+++ lib/src/sync.dart
@@ -2,35 +2,54 @@ import 'dart:mirrors';
 
 import 'serialization.dart';
 
+/// Represents the result of an operation with an endpoint
+class EndpointResult<TSyncable extends SyncableMixin> {
+  bool successful;
+}
 
-/// Represents an endpoint for syncing, such as a restful api
-abstract class SyncEndpoint<TSyncable extends SyncableMixin,
-                            TSerializer extends Serializer<TSyncable>> {
-  /// Syncs the syncable entity with the endpoint
-  Future<SyncResult> sync(TSyncable syncable);
+/// Represents the result of syncing
+class SyncResult<TSyncable extends SyncableMixin>
+    extends EndpointResult {
+
+}
+
+/// Represents an endpoint, such as a restful api
+abstract class Endpoint<TSyncable extends SyncableMixin,
+                        TSerializer extends Serializer<TSyncable>> {
+  /// Syncs a single syncable entity with the endpoint
+  Future<SyncResult<TSyncable>> sync(TSyncable syncable) async {
+    /// do push, get result of push and pull
+  }
+
+  /// Pushes a single entity and returns any updates
+  Future<EndpointResult<TSyncable>> push(TSyncable syncable);
+
+  /// Pulls and returns a single entity
+  Future<EndpointResult<TSyncable>> pull(TSyncable syncable);
 }
 
 /// Represents a restful api endpoint for syncing
 class RestfulApiSyncEndpoint<TSyncable extends SyncableMixin,
                              TSerializer extends Serializer<TSyncable>>
-                             extends SyncEndpoint {
+                             extends Endpoint {
   @override
-  Future<SyncResult> sync(syncable) {
-    // TODO: implement sync
+  Future<EndpointResult<SyncableMixin>> push(syncable) {
+    // TODO: implement push
     throw UnimplementedError();
   }
-}
 
-/// Represents the result of syncing
-class SyncResult {
-
+  @override
+  Future<EndpointResult<SyncableMixin>> pull(syncable) {
+    // TODO: implement pull
+    throw UnimplementedError();
+  }
 }
 
 /// Added to a class to support syncing
 /// Syncable classes also must be serializable
 abstract class SyncableMixin implements SerializableMixin {
   /// Syncs the entity with an endpoint
-  Future<SyncResult> sync(SyncEndpoint endpoint) async {
+  Future<SyncResult> sync(Endpoint endpoint) async {
     return await endpoint.sync(this);
   }
 
