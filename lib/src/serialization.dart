9fe1a3555543756ac5a4e6d81ff5643770731cd2
Chris Berry <chris.berry@lodlaw.com>
2020-08-07 17:07:38 +1000

    push testing done

--- lib/src/serialization.dart
+++ lib/src/serialization.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:mirrors';
 import 'dart:convert';
 
--- test/sync_test.dart
+++ test/sync_test.dart
@@ -42,19 +42,46 @@ void main() {
       expect(result, isA<EndpointResult>());
       expect(result.response, isNotNull);
       expect(result.response, isA<http.Response>());
+      expect(result.response.statusCode, equals(200));
+
       expect(result.instance, isNotNull);
       expect(result.instance, isA<TestEntity>());
+      expect(result.instance.id, equals(1));
+      expect(result.instance.name, equals('TestName'));
+      expect(result.instance.created, DateTime.parse("2020-08-07T12:30:15.123456"));
     });
 
     test('Test RestfulApiSyncEndpoint.push', () async {
+      /// Set up the mock client
       final client = MockClient();
+      final url = 'https://www.example.com/test-entity';
+      final body = '{"id": 1, "name": "TestName", "created": "2020-08-07T12:30:15.123456"}';
+      final statusCode = 200;
 
-      // Use Mockito to return an unsuccessful response when it calls the
-      // provided http.Client.
-      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
-          .thenAnswer((_) async => http.Response('Not Found', 404));
+      /// Create the endpoint and an 'outdated' instance
+      final endpoint = RestfulApiEndpoint<TestEntity>(url, client: client);
+      final instance = TestEntity(1, 'OutdatedName', DateTime.now());
+      final mockTestSerializer = TestEntitySerializer(instance:instance);
 
-      expect(await client.get('https://jsonplaceholder.typicode.com/posts/1'), isNotNull);
+      when(client.post('${url}/1', body:mockTestSerializer.toRepresentation()))
+          .thenAnswer((_) async => http.Response(body, statusCode));
+
+      /// Pull the entity using the endpoint
+      final serializer = TestEntitySerializer();
+      final result = await endpoint.push(instance, serializer);
+
+      /// Test the results of pulling the entity
+      expect(result, isNotNull);
+      expect(result, isA<EndpointResult>());
+      expect(result.response, isNotNull);
+      expect(result.response, isA<http.Response>());
+      expect(result.response.statusCode, equals(200));
+
+      expect(result.instance, isNotNull);
+      expect(result.instance, isA<TestEntity>());
+      expect(result.instance.id, equals(1));
+      expect(result.instance.name, equals('TestName'));
+      expect(result.instance.created, DateTime.parse("2020-08-07T12:30:15.123456"));
     });
   });
 }
