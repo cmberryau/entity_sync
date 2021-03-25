bba07a133a7296a33e1def0c2112d0b1d745cc05
Anh Pham <anhphamduy@outlook.com>
2021-03-25 20:41:52 +1100

    Update null safe for annotations.dart

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -3,13 +3,15 @@ import 'package:entity_sync/entity_sync.dart';
 class UseEntitySync {
   final Type baseClass;
   final List<SerializableField> fields;
-  final SerializableField keyField;
-  final SerializableField flagField;
-  final SerializableField remoteKeyField;
+  final SerializableField? keyField;
+  final SerializableField? flagField;
+  final SerializableField? remoteKeyField;
 
-  const UseEntitySync(this.baseClass,
-      {this.fields = const [],
-      this.keyField,
-      this.flagField,
-      this.remoteKeyField});
+  const UseEntitySync(
+    this.baseClass, {
+    this.fields = const [],
+    this.keyField,
+    this.flagField,
+    this.remoteKeyField,
+  });
 }
